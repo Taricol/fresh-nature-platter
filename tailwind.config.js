212e7fe47c6abf782bdee7218421ce7d396491d1
Taricol <mozumder9122023@gmail.com>
2025-05-18 23:29:56 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                registerprimary: "#179800",
+            },
+          },
+    },
+    plugins: [],
+  }
